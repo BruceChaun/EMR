20a48c807ed54e5543ffb5ea8e15fe087bd61e6a
BruceChaun <guiqicj@gmail.com>
2016-05-25 09:26:34 +0800

    update script

--- emr.sh
+++ emr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e 
+
 path1=build
 path2=com/emr/nlp/dic
 
@@ -9,7 +11,7 @@ fi
 
 # java compile
 
-javac -classpath lib/apache-commons-lang.jar:lib/commons-configuration-1.9.jar:lib/commons-logging-1.2.jar:lib/hadoop-core-1.2.1.jar:lib/hadoop-mapreduce-client-core-0.23.1.jar:lib/IKAnalyzer2012_u6.jar:lib/jackson-all-1.9.0.jar:lib/lucene-core-3.6.0.jar:lib/mysql-connector-java-5.1.23-bin.jar -d build/ src/com/emr/dbutil/IndexDB.java src/com/emr/nlp/NLP.java src/com/emr/mapred/Index.java src/com/emr/mapred/EMRChain.java 
+javac -classpath lib/apache-commons-lang.jar:lib/commons-configuration-1.9.jar:lib/commons-logging-1.2.jar:lib/hadoop-core-1.2.1.jar:lib/hadoop-mapreduce-client-core-0.23.1.jar:lib/IKAnalyzer2012_u6.jar:lib/jackson-all-1.9.0.jar:lib/lucene-core-3.6.0.jar:lib/mysql-connector-java-5.1.23-bin.jar -d build/ src/com/emr/dbutil/IndexDB.java src/com/emr/entity/IndexInfo.java src/com/emr/nlp/NLP.java src/com/emr/mapred/Index.java src/com/emr/mapred/EMRChain.java
 
 if [ ! -d $path1/$path2 ] ; then
     mkdir $path1/$path2
