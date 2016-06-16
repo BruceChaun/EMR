3a2c3d884e93b2674c80f9faf59c548a917854c6
BruceChaun <guiqicj@gmail.com>
2016-06-16 15:15:13 +0800

    init data

--- src/com/emr/entity/Data.java
+++ src/com/emr/entity/Data.java
@@ -2,14 +2,20 @@ package com.emr.entity;
 
 import java.util.ArrayList;
 
+import com.emr.preprocess.Regex;
+
 public class Data {
-	private ArrayList<Integer> missingIndex = new ArrayList();
+	private ArrayList<Integer> missingIndex = new ArrayList<Integer>();
 	
 	public double[] data;
 	
 	public void init(String[] data) {
 		int n = data.length;
+		this.data = new double[n];
 		for (int i = 0; i < n; i++) {
+			if (!data[i].isEmpty())
+				this.data[i] = Regex.parseNumeric(data[i]);
+			
 			if (data[i].isEmpty()) {
 				missingIndex.add(i);
 			}
